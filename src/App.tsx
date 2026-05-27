58f884896005b91e4f74830826aff13b52dbec60
whitewind <whitewind36462201@outlook.com>
2026-05-27 15:59:07 +0800

    Create Tauri React TypeScript app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="app-shell">
+      <h1>SkyMusicPlay Lite</h1>
+      <p>App is running</p>
+    </main>
+  );
+}
+
+export default App;
